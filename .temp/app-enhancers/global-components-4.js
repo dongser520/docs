7a5dd2d55fc51a25d542b89c8e24b0c000cecbaf
dongser520 <yd10001@qq.com>
2023-09-27 10:59:00 +0800

    feat:更新课堂文档

--- .temp/app-enhancers/global-components-4.js
+++ .temp/app-enhancers/global-components-4.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 Vue.component("CountUp", () => import("E:\\vuepress\\vuepress-github\\docs\\docs\\.vuepress\\components\\CountUp"))
 
 Vue.component("Badge", () => import("E:\\vuepress\\vuepress-github\\docs\\node_modules\\@vuepress\\theme-default\\global-components\\Badge"))
-Vue.component("CodeGroup", () => import("E:\\vuepress\\vuepress-github\\docs\\node_modules\\@vuepress\\theme-default\\global-components\\CodeGroup"))
 Vue.component("CodeBlock", () => import("E:\\vuepress\\vuepress-github\\docs\\node_modules\\@vuepress\\theme-default\\global-components\\CodeBlock"))
+Vue.component("CodeGroup", () => import("E:\\vuepress\\vuepress-github\\docs\\node_modules\\@vuepress\\theme-default\\global-components\\CodeGroup"))
 
 
 export default {}
